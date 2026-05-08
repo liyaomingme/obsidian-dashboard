2ec891787d88553774cffcf8e87e75eb553a59ae
liyaoming <liyaoming.me@gmail.com>
2026-05-08 17:08:46 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -272,16 +272,14 @@ class QuickNoteModal extends Modal {
         this.onSubmit = onSubmit; 
         this.title = `${moment().format('MMDD')}-`; 
         this.folderPath = config.folder.replace(/\{\{YYYY\}\}/g, moment().format('YYYY')).replace(/\{\{MM\}\}/g, moment().format('MM'));
-        
-        // 🌟 核心闪烁修复：在元素附加到 DOM 之前注入 CSS 类 🌟
-        // 这样弹窗在渲染的第一帧就拥有 opacity:0 和高级入场动画，绝不突兀！
-        this.containerEl.addClass('ios-glass-modal-container');
-        this.modalEl.addClass('ios-glass-modal');
     }
     
     onOpen() {
-        const { contentEl } = this;
-        // 原本在这里的 addClass 被移到了上方的 constructor 中
+        const { contentEl, modalEl, containerEl } = this;
+        
+        // 🌟 退回原生的生命周期：在 onOpen 时赋予基础样式类 🌟
+        containerEl.addClass('ios-glass-modal-container');
+        modalEl.addClass('ios-glass-modal');
         
         contentEl.createEl('h3', { text: this.actionConfig.name });
         
@@ -334,15 +332,6 @@ class QuickNoteModal extends Modal {
             this.close(); 
             this.onSubmit(this.title, this.date, this.folderPath); 
         }));
-    }
-
-    // 优雅退场：拦截关闭事件，赋予退场动画时间
-    close() {
-        this.modalEl.addClass('is-closing');
-        this.containerEl.addClass('is-closing');
-        setTimeout(() => {
-            super.close();
-        }, 300);
     }
 }
 
